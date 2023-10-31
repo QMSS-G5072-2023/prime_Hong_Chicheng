b4c3fba6f9c2d3ebffd116cc19723e114ed52dca
shong0210 <sh4432@columbia.edu>
2023-10-31 19:33:18 -0400

    updated pytest

--- prime_sh4432/tests/test_prime_sh4432.py
+++ prime_sh4432/tests/test_prime_sh4432.py
@@ -1,1 +1,11 @@
 from prime_sh4432 import prime_sh4432
+from from src.prime_sh4432.is_prime import is_prime
+def test_is_prime():
+    prime_numbers=[2,7,13,19]
+    composite_numbers=[8,9,15]
+    for prime in prime_numbers:
+        assert is_prime(prime), f"{prime} is not recognized as prime."
+    for composite in composite_numbers:
+        assert not is_prime(composite), f"{composite} is recognized as prime, but it's composite."
+    print("Tests passed.")
+    
